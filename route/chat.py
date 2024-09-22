cba6d08531048085907de5996e6a806ab87c8473
andres-saa <andrew19f@gmail.com>
2024-09-22 10:16:49 -0500

    mejor

--- route/chat.py
+++ route/chat.py
@@ -6,19 +6,12 @@ from pydantic import BaseModel
 
 chat_router = APIRouter()
 
-
-
 productos_disponibles = [
     {"id": 1, "nombre": "hamburguesa", "precio": 12000},
     {"id": 2, "nombre": "pizza", "precio": 18000},
     {"id": 3, "nombre": "perro caliente", "precio": 10000},
     {"id": 4, "nombre": "papas fritas", "precio": 5000}
 ]
-
-
-
-
-
 
 base = {
     "pedido": {   
@@ -48,7 +41,6 @@ base = {
             "¡Buenas tardes! ¿Cómo puedo asistirte?",
         ]
     },
-   
     "despedidas": {
         "patterns": [
             ['adios'],
@@ -77,117 +69,116 @@ base = {
     }
 }
 
-
-
 class UserInput(BaseModel):
     answer: str  # Respuesta del usuario
-    question: str  # Pregunta previa que se le hizo al usuario
-    client_id: str = None  # ID del cliente para verificar si está registrado
+    client_id: str  # ID del cliente para verificar si está registrado
 
 # Iniciar el archivo data.json si no existe
 def init_data():
     if not os.path.exists("data.json"):
         data = {
             "clientes_registrados": {},
-            "productos_disponibles": [
-                {"id": 1, "nombre": "hamburguesa", "precio": 12000},
-                {"id": 2, "nombre": "pizza", "precio": 18000},
-                {"id": 3, "nombre": "perro caliente", "precio": 10000},
-                {"id": 4, "nombre": "papas fritas", "precio": 5000}
-            ],
-            "carrito": {}
+            "productos_disponibles": productos_disponibles,
+            "carrito": {},
+            "ultima_respuesta": ""  # Solo una última respuesta
         }
         with open("data.json", "w") as file:
             json.dump(data, file, indent=4)
     else:
         with open("data.json", "r") as file:
             return json.load(file)
+        
+
+
+
 
 def load_data():
     if not os.path.exists("data.json"):
         return init_data()
     with open("data.json", "r") as file:
         return json.load(file)
+    
+def load_data_responses():
+    with open("responses.json", "r") as file:
+        return json.load(file)
 
 def save_data(data):
     with open("data.json", "w") as file:
         json.dump(data, file, indent=4)
 
+def save_data_responses(data):
+    with open("responses.json", "w") as file:
+        json.dump(data, file, indent=4)
+
 data = load_data()
+data_responses = load_data_responses()
 
-def generate_client_id():
-    return str(random.randint(1000000, 9999999))
+print(data_responses)
 
-def register_client(user_info):
+def register_client(user_info, client_id):
     info_parts = user_info.split('\n')
     client_data = {}
-    client_id = generate_client_id()  # Generar un nuevo ID para el cliente
     for part in info_parts:
         key, value = part.split(': ')
         client_data[key.strip().lower()] = value.strip()
-    data["clientes_registrados"][client_id] = client_data
+    
+    data["clientes_registrados"][client_id] = client_data  # Usar el ID proporcionado
     save_data(data)
-    return client_id, "¡Registro completado exitosamente! Aquí está tu ID de cliente: " + client_id
+    return "¡Registro completado exitosamente! Aquí está tu ID de cliente: " + client_id
 
 def check_client_registration(client_id):
     return client_id in data["clientes_registrados"]
 
+def get_previous_question(user_id:str):
+    return data_responses[user_id]
 
-def match_pattern(user_input: str, previous_question: str, client_id: str):
+def update_last_response(response,user_id:str):
+    # Actualiza solo la última respuesta
+    data_responses[user_id] = response
+    save_data_responses(data_responses)
+
+def match_pattern(user_input: str, client_id: str):
     user_input_lower = user_input.lower()
     
- 
-    if previous_question.startswith("Parece que aún no estás registrado"):
-        client_id, response = register_client(user_input)
+    # Obtener la última respuesta desde el archivo
+    previous_question = get_previous_question(client_id)
+    
+    if previous_question and previous_question.startswith("Parece que aún no estás registrado"):
+        response = register_client(user_input, client_id)
+        update_last_response(response,client_id)
         return response
     
-    
-
     user_input_set = set(user_input_lower.split())
     for pattern in base['pedido']['patterns']:
-            print('filtro 1')
-            pattern_set = set(pattern)
-            if pattern_set.issubset(user_input_set):
-                if  client_id is None or not check_client_registration(client_id):
-                    print('pedido iniciado')
-                    return "Parece que aún no estás registrado. Por favor, proporciona tu información para registrarte. http://127.0.0.1:5500/info/info.html"
+        pattern_set = set(pattern)
+        if pattern_set.issubset(user_input_set):
+            if not check_client_registration(client_id):
+                update_last_response("Parece que aún no estás registrado. Por favor, proporciona tu información para registrarte.",client_id)
+                return "Parece que aún no estás registrado. Por favor, proporciona tu información para registrarte."
+
+            response = random.choice(base['pedido']['responses'])
+            update_last_response(response,client_id)  # Actualiza la última respuesta
+            return response
+
+
+    for category, content in base.items():
+        if category != 'pedido':  # Evitar duplicar la comprobación de 'pedido'
+            for pattern in content['patterns']:
+                pattern_set = set(pattern)
+                if pattern_set.issubset(user_input_set):
+                    
+                    response  = random.choice(content['responses'])
+                    update_last_response(response,client_id)  # Actualiza la última respuesta
+                    return response
                 
-                return random.choice(base['pedido']['responses'])
-    
-
-
-
-   
-    if previous_question in base['pedido']['responses']:
-        # Convertir la entrada del usuario en un conjunto de palabras
-        user_input_set = set(user_input_lower.split())
-
-        # Verificar si alguna de las listas de patrones de 'pedido' está completamente contenida en la entrada del usuario
-        for pattern in base['pedido']['patterns']:
-            print('filtro 1')
-            pattern_set = set(pattern)
-            if pattern_set.issubset(user_input_set):                
-                return random.choice(base['pedido']['responses'])
-                
-
-
-    # # Verificar y responder según otros patrones de entrada
-    # for category, content in base.items():
-    #     for pattern in content['patterns']:
-    #         pattern_str = ' '.join(pattern)
-    #         if pattern_str in user_input_lower:
-    #             return random.choice(content['responses'])
-            
-
-
-    
-
+    update_last_response("Lo siento, no entiendo tu solicitud. ¿Puedes repetirla?",client_id)
     return "Lo siento, no entiendo tu solicitud. ¿Puedes repetirla?"
 
 @chat_router.post('/chat')
 def chatbot(userInput: UserInput):
-    response = match_pattern(userInput.answer, userInput.question, userInput.client_id)
+    if not data_responses.get(userInput.client_id):
+        update_last_response('hola',userInput.client_id)
+    response = match_pattern(userInput.answer, userInput.client_id)
     return {"response": response}
 
-# Ejecutar init_data al iniciar para asegurar que data.json está presente
 init_data()
