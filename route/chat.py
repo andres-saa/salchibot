2c63b59b6d49b90777bcf1378aa04bb2259bbd98
andres-saa <andrew19f@gmail.com>
2024-10-05 19:20:25 -0500

    bot case ready

--- route/chat.py
+++ route/chat.py
@@ -379,7 +379,7 @@ def extraer_productos(texto,wsp_id):
             resultado.append(linea.title())  # Capitaliza la primera letra de cada palabra
         
         # Añadir el precio del domicilio
-        resultado.append(f"\nDomicilio = ${int(delivery_price):,}")
+        resultado.append(f"Domicilio = ${int(delivery_price):,}")
         
         # Añadir el total sumando el domicilio
         total += delivery_price
@@ -390,7 +390,7 @@ def extraer_productos(texto,wsp_id):
     print(productos_con_id)
     
     chabot_instance.create_temp_order(wsp_id=wsp_id,json_data=order_json)
-    return f"Listo papi, Ya tengo tu pedido registrado\n\n{convertir_pedido(productos_finales,delivery_price)} \n {get_my_data(wsp_id)} \n\n si todo es correcto porfa ingresa la palabra *confirmar* para enviarlo a preparacion si necesitas cambiar la direccion registrada o el metodo de pago puedes hacerlo aqui https://bot.salchimonster.com/registro/"
+    return f"Listo papi, Ya tengo tu pedido registrado\n\n{convertir_pedido(productos_finales,delivery_price)}\n{get_my_data(wsp_id)}\n\nsi todo es correcto porfa ingresa la palabra *confirmar* para enviarlo a preparacion si necesitas cambiar la direccion registrada o el metodo de pago puedes hacerlo aqui https://bot.salchimonster.com/registro/"
 
 
 def generar_mensaje_pedido(pedido):
