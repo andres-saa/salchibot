428c1ad27f91c0056e81da2f51ac894004690b6c
andres-saa <andrew19f@gmail.com>
2024-11-04 08:46:42 -0500

    nueva interfaz

--- cata/src/router/index.js
+++ cata/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import App from '../App.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -7,7 +7,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
 
-      component: () => import('../App.vue'),
+      component: App,
       children: [
 
         {
